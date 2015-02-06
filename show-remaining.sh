67eb133d0c6f4fdb6969795f90e1728d4086ade3
Tim Bird <tim.bird@sonymobile.com>
2015-02-06 11:16:03 -0800

    Add show-remaining script to show the amount of data left to sync

---
+++ show-remaining.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo -n "Files remaining to transfer "
+diff -u ../source_index ../dest_index | grep ^-f | cut -d , -f 3 | wc -l
+echo -n "Size remaining to transfer "
+diff -u ../source_index ../dest_index | grep ^-f | cut -d , -f 3 | ./add -q -u
+
+if [ "$1" = "-v" ];  then
+	diff -u ../source_index ../dest_index | grep ^-f | cut -d , -f 2,3 ;
+fi
