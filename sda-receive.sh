ebec652ad42a2b6c425c5f925f01d7aa91202d2d
Tim Bird <tim.bird@sonymobile.com>
2015-02-06 08:11:16 -0800

    Adjust receive and send helper scripts

--- sda-receive.sh
+++ sda-receive.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./dropsync -p 120 -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
+./dropsync -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
+sleep 10
+./dropsync -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log -i
