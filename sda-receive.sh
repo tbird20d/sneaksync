014bb49fabd8c1529a433befc7385c138e60f607
Tim Bird <tim.bird@sonymobile.com>
2015-03-16 10:38:31 -0700

    Use -p option in sda-receive shell script, instead of just using sleep

--- sda-receive.sh
+++ sda-receive.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-./dropsync -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
-sleep 10
-./dropsync -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log -i
+./dropsync -p 30 -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
