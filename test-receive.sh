fdb3e21d035867d1ff48340e0c5e112ad0e749b7
Tim Bird <tim.bird@sonymobile.com>
2015-03-17 12:29:41 -0700

    Allow use of simplified configs for sender and receiver
    Previously, parts of the code assumed that both sender and receiver
    have all config items configured (both source_dir and dest_dir).  This
    code separates out references to both of those, depending on the
    program mode, so that that configuration (particularly for the
    receiver) doesn't need to include attributes that are not needed.
    
    The config files really should be separate anyway.  In the early
    releases of this program, a shared config was used for testing,
    but in real usage the configs will almost never be the same, because
    the sync directory will likely be different on different machines.

--- test-receive.sh
+++ test-receive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#./sneaksync -p 2 -v -r -c sda-test.conf -l sda-test.log
-./sneaksync -v -r -c sda-test.conf -l sda-test.log
+#./sneaksync -p 2 -v -r -c sda-test-receive.conf -l sda-test.log
+./sneaksync -v -r -c sda-test-receive.conf -l sda-test.log
--- test-send.sh
+++ test-send.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#./sneaksync -p 2 -v -c sda-test.conf -l sda-test.log
-./sneaksync -v -c sda-test.conf -l sda-test.log
+#./sneaksync -p 2 -v -c sda-test-send.conf -l sda-test.log
+./sneaksync -v -c sda-test-send.conf -l sda-test.log
