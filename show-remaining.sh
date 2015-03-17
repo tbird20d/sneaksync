47267a0e2351c556d49ff2334224a9a72958cc51
Tim Bird <tim.bird@sonymobile.com>
2015-03-16 21:35:31 -0700

    Show more info in show-remaining.sh
    Add parts and directories to output

--- show-remaining.sh
+++ show-remaining.sh
@@ -3,6 +3,10 @@
 
 echo -n "Files remaining to transfer "
 diff -u ../source_index ../dest_index | grep ^-f | cut -d , -f 3 | wc -l
+echo -n "Parts remaining to transfer "
+diff -u ../source_index ../dest_index | grep ^-p | cut -d , -f 3 | wc -l
+echo -n "Dirs  remaining to transfer "
+diff -u ../source_index ../dest_index | grep ^-d | cut -d , -f 3 | wc -l
 echo -n "Size remaining to transfer "
 diff -u ../source_index ../dest_index | grep ^-f | cut -d , -f 3 | ./add -q -u
 
