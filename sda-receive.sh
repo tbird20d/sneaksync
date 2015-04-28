90ba46502314c1ed2a179dc22777f06f836ea2ca
Tim Bird <tim.bird@sonymobile.com>
2015-04-28 07:47:12 -0700

    Remove loop from receiver invocation in wrapper script

--- sda-receive.sh
+++ sda-receive.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./sneaksync -p 30 -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
+./sneaksync -a -c ../sda-timdesk-receiver.conf -r -v -l sda-tim-home-work-receiver.log
